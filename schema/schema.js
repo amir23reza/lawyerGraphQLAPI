00988bad37520221a0d1bb22a3e1c5ff6f5cb5b8
amir23reza <amir.reza.haghverdi.khu@gmail.com>
2019-02-03 22:52:11 +0330

    a

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,10 @@
 const graphQL = require('graphql');
 
 const {GraphQLObjectType , GraphQLString} = graphQL;
-
+// a comment for github changing code checking
 const USER = new GraphQLObjectType({
     name : 'user' , 
     fields : () => {
-        name : GraphQLString
+        id : GraphQLString  
     }
 })
